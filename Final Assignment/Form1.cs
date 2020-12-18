39565b2668981c196303d091f4fd8260f8bf0f9a
shahilpatwary <74975968+shahilpatwary@users.noreply.github.com>
2020-12-18 15:34:34 +0600

    From1 Exit

--- Final Assignment/Form1.cs
+++ Final Assignment/Form1.cs
@@ -16,5 +16,10 @@ namespace Final_Assignment
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
