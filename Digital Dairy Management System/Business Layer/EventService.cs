21a7e23e03c7bc36a3e2fbb878448b7768d177c1
shahilpatwary <74975968+shahilpatwary@users.noreply.github.com>
2020-12-19 16:42:34 +0600

    EventService(Business Layer)

---
+++ Digital Dairy Management System/Business Layer/EventService.cs
@@ -0,0 +1,24 @@
+﻿using Digital_Dairy_Management_System.Data_Access_Layer;
+using Digital_Dairy_Management_System.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Dairy_Management_System.Business_Layer
+{
+    class EventService
+    {
+        EventDataAccess eventDataAccess;
+        public EventService()
+        {
+            this.eventDataAccess = new EventDataAccess();
+        }
+
+        public List<Event> GetEventList()
+        {
+            return this.eventDataAccess.GetAllEvent();
+        }
+    }
+}
