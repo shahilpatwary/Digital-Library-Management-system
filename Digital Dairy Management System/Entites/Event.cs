4b274b67cc5f3056527523bebe7f5bf194dad430
shahilpatwary <74975968+shahilpatwary@users.noreply.github.com>
2020-12-19 14:03:35 +0600

    Entities
    class add

---
+++ Digital Dairy Management System/Entites/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Dairy_Management_System.Entites
+{
+    class Event
+    {
+        public int EventId { get; set; }
+        public int EventName { get; set; }
+    }
+}
---
+++ Digital Dairy Management System/Entites/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Dairy_Management_System.Entites
+{
+    class Note
+    {
+        public int NoteId { get; set; }
+        public string NoteName { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Importance { get; set; }
+        public string Description { get; set; }
+        public int EventId { get; set; }
+    }
+}
